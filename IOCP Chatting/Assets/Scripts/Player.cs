9bae7675d504cfc517905679995f24a0adbdcaa8
Chrivent <yechandubai@gmail.com>
2023-12-12 01:47:02 +0900

    asdf

--- IOCP Chatting/Assets/Scripts/Player.cs
+++ IOCP Chatting/Assets/Scripts/Player.cs
@@ -9,12 +9,36 @@ public class Player : MonoBehaviour
     public bool IsLocal;
     public PlayerMovement Movement;
 
+    void Start()
+    {
+        gameObject.layer = LayerMask.NameToLayer("Player");
+    }
+
     // Update is called once per frame
     void FixedUpdate()
     {
         if (Movement != null && IsLocal)
         {
             Movement.Move();
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 startPos = transform.position + transform.TransformDirection(Vector3.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(startPos, transform.TransformDirection(Vector3.forward), out hit, Mathf.Infinity, 1 << LayerMask.NameToLayer("Player")))
+            {
+                Debug.DrawRay(startPos, transform.TransformDirection(Vector3.forward) * 1000, Color.red);
+                Debug.Log("Player Hit");
+            }
+            else
+            {
+                Debug.DrawRay(transform.position, transform.TransformDirection(Vector3.forward) * 1000, Color.green);
+                Debug.Log("Shot");
+            }
         }
     }
 }
